0b486f75d6ef2c7f2ebe7d6f2c095d240a1ec094
ruiwang <ruiwang.bh@hotmail.com>
2021-04-20 14:18:37 -0400

    Login, Login_status, Logout, Signup

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,57 @@
 from django.contrib.auth.models import User
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+# serializer用处
+# 1 前端去渲染用户object成为一个json格式的数据
+# 2 验证用户的输入，去做validation。检测用户输入是否按照要求进行，返回相应的response
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email')
+        fields = ('username', 'email')
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'username': 'This username has been occupied.'
+            })
+        return data
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    # will be called when is_valid is called
+    def validate(self, data):
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'username': 'This username has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'email': 'This email address has been occupied.'
+            })
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        password = validated_data['password']
+        email = validated_data['email'].lower()
+        user = User.objects.create_user(
+            username = username,
+            email = email,
+            password = password,
+        )
+        return user
