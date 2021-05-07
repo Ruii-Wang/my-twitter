c4f55818d4fc23c17ee29cd503b383ccb8b17355
ruiwang <ruiwang.bh@hotmail.com>
2021-05-07 14:28:35 -0400

    add api and unit tests

--- tweets/api/tests.py
+++ tweets/api/tests.py
@@ -10,7 +10,8 @@ TWEET_CREATE_API = '/api/tweets/'
 class TweetApiTests(TestCase):
 
     def setUp(self):
-        self.anonymous_client = APIClient()
+        # 匿名用户 其创建已经挪到testcase当中
+        # self.anonymous_client = APIClient()
 
         self.user1 = self.create_user('user1', 'user1@jiuzhang.com')
         self.tweets1 = [self.create_tweet(self.user1) for i in range(3)]
