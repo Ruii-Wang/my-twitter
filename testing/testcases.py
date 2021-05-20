9282a7244ac9790f96da8d6e160a53223510aa9e
ruiwang <ruiwang.bh@hotmail.com>
2021-05-20 18:33:29 -0400

    add likes create api

--- testing/testcases.py
+++ testing/testcases.py
@@ -47,3 +47,11 @@ class TestCase(DjangoTestCase):
             object_id = target.id,
             user = user,
         )
+        return instance
+
+    def create_user_and_client(self, *args, **kwargs):
+        user = self.create_user(*args, **kwargs)
+        client = APIClient()
+        client.force_authenticate(user)
+        return user, client
+
