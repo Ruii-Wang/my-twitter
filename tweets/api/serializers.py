46369002ae44eb4b347dc13ffdeae4cfc2ad49a7
ruiwang <ruiwang.bh@hotmail.com>
2021-05-20 15:57:57 -0400

    add tweet retrieve api

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from tweets.models import Tweet
 from accounts.api.serializers import UserSerializerForTweet
+from comments.api.serializers import CommentSerializer
 
 class TweetSerializer(serializers.ModelSerializer):
     user = UserSerializerForTweet()
@@ -22,3 +23,14 @@ class TweetSerializerForCreate(serializers.ModelSerializer):
         content = validated_data['content']
         tweet = Tweet.objects.create(user=user, content=content)
         return tweet
+
+
+
+class TweetSerializerWithComments(serializers.ModelSerializer):
+    user = UserSerializerForTweet()
+    # <Homework> 使用serializers.SerializerMethodField的方式实现comments
+    comments = CommentSerializer(source='comment_set', many=True)
+
+    class Meta:
+        model = Tweet
+        fields = ('id', 'user', 'comments', 'created_at', 'content')
