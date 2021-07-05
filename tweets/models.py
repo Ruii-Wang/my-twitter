cbd500d126ddffe61780e5c59a4dfdf0e71e3bd4
ruiwang <ruiwang.bh@hotmail.com>
2021-07-05 18:09:48 -0400

    cache tweet list to redis

--- tweets/models.py
+++ tweets/models.py
@@ -7,6 +7,7 @@ from tweets.constants import TweetPhotoStatus, TWEET_PHOTO_STATUS_CHOICES
 from utils.memcached_helper import MemcachedHelper
 from django.db.models.signals import post_save
 from utils.listeners import invalidate_object_cache
+from tweets.listeners import push_tweet_to_cache
 
 
 class Tweet(models.Model):
@@ -87,3 +88,4 @@ class TweetPhoto(models.Model):
 
 
 post_save.connect(invalidate_object_cache, sender = Tweet)
+post_save.connect(push_tweet_to_cache, sender=Tweet)
